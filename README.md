1af320edd24a7896aeef8e38820ce5f2fc7f39f3
Sridhar <b.sridhar85@gmail.com>
2021-02-09 20:07:19 +0000

    added security & updated readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ebi
+## Person service
+
+This is a sample project to demonstrate person service. 
+
+###### Commands to build & run this application
+```
+cd person-service
+mvn clean install
+
+sudo docker build -t person-service .
+sudo docker run -p 8080:8080 person-service  
+```
+
+Once the server is up & running then go to
+
+> http://localhost:8080/swagger-ui/
+
+###### The delete endpoint is secured with basic authentication. Use the credentials user/user.
---
+++ person-service/src/main/java/com/ebi/person_service/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.ebi.person_service.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.DELETE).authenticated().and().httpBasic();
+	}
+
+}
